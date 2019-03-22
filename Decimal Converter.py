b8e9c24f54043d9456b3233417487883b46d1ed6
Jakub Łodej <kubula.17@gmail.com>
2019-03-22 12:23:04 +0100

    Add files via upload
    Uploaded py version

---
+++ Decimal Converter.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+#Decimal Converter
+
+decimal=int(input("Put your decimal value: "))
+print("The decimal value of",decimal,"is:")
+print(bin(decimal),"in binary.")
+print(oct(decimal),"in octal.")
+print(hex(decimal),"in hexadecimal.")
+
